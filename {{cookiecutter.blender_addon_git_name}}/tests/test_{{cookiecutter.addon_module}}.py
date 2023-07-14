4cc3fbf3bdf715988740ffaee3213228a0edc7ee
Nig3l <nig3lpro@gmail.com>
2023-07-14 19:21:00 +0200

    Add Blender tests

---
+++ {{cookiecutter.blender_addon_git_name}}/tests/test_{{cookiecutter.addon_module}}.py
@@ -0,0 +1,9 @@
+import unittest
+
+class Test_{{cookiecutter.addon_module}}(unittest.TestCase):
+
+    def test_dummy(self):
+        self.assertTrue(False)
+
+if __name__ == '__main__':
+    unittest.main()
